4d5680bc69c723e59bc77b9e6f6cdcc85cc75f4e
tamina <tamina0624@yahoo.co.jp>
2026-05-03 18:56:50 +0900

    EC2インスタンスを作成しApacheで初期ページを表示

--- projects/study_terraform_20260421/main.tf
+++ projects/study_terraform_20260421/main.tf
@@ -93,3 +93,47 @@ resource "aws_security_group" "portfolio_sg" {
     Name = "${var.project_name}-sg"
   }
 }
+
+# 最新のAmazon Linux 2023のAMI（イメージ） IDを自動取得
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+}
+
+# キーペアの登録
+resource "aws_key_pair" "portfolio_key" {
+  key_name   = "portfolio-key"
+  public_key = file("./my-portfolio-key.pub") # 公開鍵を読み込む
+}
+
+# EC2インスタンスの作成
+resource "aws_instance" "portfolio_web" {
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t2.micro" # 無料枠対象
+
+  # 作成したパブリックサブネットに配置
+  subnet_id = aws_subnet.public_sub_1a.id
+
+  associate_public_ip_address = true
+
+  # 作成したセキュリティグループを適用
+  vpc_security_group_ids = [aws_security_group.portfolio_sg.id]
+
+
+  key_name = aws_key_pair.portfolio_key.key_name # 鍵を紐付け
+
+  # サーバーに名前をつける
+  tags = {
+    Name = "${var.project_name}-web-server"
+  }
+}
+
+# 実行後にサーバーのパブリックIPを表示させる設定
+output "instance_public_ip" {
+  value = aws_instance.portfolio_web.public_ip
+}

4b21f49b1d9389b461fe7307390860c98a830d12
tamina <tamina0624@yahoo.co.jp>
2026-05-03 20:12:19 +0900

    セキュリティ機能有効化コマンドの追記

--- projects/study_terraform_20260421/modules/compute/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "project_name" {}
-variable "vpc_id" {}
-variable "subnet_id" {}
-variable "aws_security_group_id" {}
--- projects/study_terraform_20260421/modules/network/output.tf
+++
@@ -1,2 +0,0 @@
-output "vpc_id" { value = aws_vpc.main.id }
-output "subnet_id" { value = aws_subnet.public.id }

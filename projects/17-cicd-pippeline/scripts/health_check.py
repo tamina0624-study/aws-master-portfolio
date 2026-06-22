7012ea558ef60b38a6856298af9b93ca0727f4e6
tamina0624-study <tamina0624@yahoo.co.jp>
2026-06-22 22:26:31 +0900

    CICDパイプラインの調査2

--- projects/17-cicd-pippeline/scripts/health_check.py
+++ projects/17-cicd-pippeline/scripts/health_check.py
@@ -4,9 +4,8 @@
 import sys
 import json
 import time
-from typing import Dict, List
 
-def check_s3_bucket(bucket_name: str) -> Dict[str, bool]:
+def check_s3_bucket(bucket_name: str) -> dict[str, bool]:
     """Check S3 bucket accessibility and basic properties."""
     import boto3
     from botocore.exceptions import ClientError

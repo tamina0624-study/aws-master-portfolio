9589d98f50cc8ecbaa72b75d4d3acbaca394fdac
tamina0624-study <tamina0624@yahoo.co.jp>
2026-06-23 22:09:08 +0900

    Add S3 public access block for app bucket

--- projects/17-cicd-pippeline/terraform/main.tf
+++ projects/17-cicd-pippeline/terraform/main.tf
@@ -6,6 +6,15 @@ resource "aws_s3_bucket" "app_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "app_bucket_pab" {
+  bucket = aws_s3_bucket.app_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_versioning" "app_bucket_versioning" {
   bucket = aws_s3_bucket.app_bucket.id
 

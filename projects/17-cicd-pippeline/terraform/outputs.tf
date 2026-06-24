9aa5674a1a650c8c010454455f72f5567c163ee6
tamina0624-study <tamina0624@yahoo.co.jp>
2026-06-25 00:39:09 +0900

    terraformバグ修正

--- projects/17-cicd-pippeline/terraform/outputs.tf
+++ projects/17-cicd-pippeline/terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
-  value       = local.bucket_name
+  value       = aws_s3_bucket.app_bucket.id
 }
 
 output "s3_bucket_arn" {
   description = "ARN of the S3 bucket"
-  value       = local.bucket_arn
+  value       = aws_s3_bucket.app_bucket.arn
 }
 
 output "aws_account_id" {
